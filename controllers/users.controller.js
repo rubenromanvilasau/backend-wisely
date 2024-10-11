bd1136b7a72f785756e35e7da3310820716814b2
RubenRoman <ruben.roman@forcast.cl>
2024-10-11 00:14:28 -0300

    :bug: fix get users endpoint

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,7 +3,7 @@ const User = require('../models/user.model');
 const getUsers = async(req, res, next) => {
     try {
         const users = await User.findAll();
-        res.send(users);
+        res.send({data: users});
     } catch (error) {
         next(error);
     }
